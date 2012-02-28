7852c54a87221846c8edc00e981b7e682a96b71a
amar singh <aspatial.singh@gmail.com>
2012-02-28 13:32:00 +0530

    created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commerce::Application.config.secret_token = '8e83fcf5e31c430ce13cce56cf871a8318ca337a08a135fb7d6d2862b7fac2c6bf9105b9d65df56cca56dd19e5cde91ca8089c086beabeb19f9ccad32e4b122f'
